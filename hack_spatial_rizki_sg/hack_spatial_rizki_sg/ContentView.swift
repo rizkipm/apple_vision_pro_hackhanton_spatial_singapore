5e671324e4b7db6acc1287836edd8315e076226a
Rizki Pm <udarizki2020@gmail.com>
2025-08-05 14:34:42 +0800

    update

--- hack_spatial_rizki_sg/hack_spatial_rizki_sg/ContentView.swift
+++ hack_spatial_rizki_sg/hack_spatial_rizki_sg/ContentView.swift
@@ -10,15 +10,89 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+        HStack{
+            Image("rizki")
+                .resizable()
+                .frame(width: 350, height: 500)
+                .padding()
+                .background(.gray.opacity(90))
+                .opacity(90)
+                .cornerRadius(10)
+            
+            Image("building")
+                .resizable()
+                .frame(width: 350, height: 500)
+                .padding()
+                .background(.gray.opacity(90))
+                .opacity(90)
+                .cornerRadius(10)
+            
+        }
+        
+    
+        HStack {
+            Text("Hello Everyone!, ")
+            Image(systemName: "hand.wave")
+            Text("I am Rizki Syaputra & I Love Photography")
         }
         .padding()
+        .background(.blue)
+        .opacity(50)
     }
+    
+//    var body: some View {
+//        HStack {
+//            Image(systemName: "face.smiling")
+//            Text("Jia Chen")
+//        }
+//    }
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "face.smiling")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Rizki Syaputra")
+//        }
+        
+//        VStack {
+//            Text("I")
+//            Image(systemName: "heart")
+//            Text("SwiftUI")
+//        }
+//
+        
+//        HStack {
+//            Text("I")
+//            Image(systemName: "heart")
+//            Text("SwiftUI")
+//        }
+        
+//        HStack {
+//            Text("I")
+//            Image(systemName: "heart")
+//            Text("SwiftUI")
+//        }
+        
+//        HStack {
+//            Text("I")
+//            Image(systemName: "heart")
+//            Spacer()
+//            Text("SwiftUI")
+//        }
+        
+//        VStack {
+//            Model3D(named: "Scene", bundle: realityKitContentBundle)
+//                .padding(.bottom, 50)
+//
+//            Text("Hello, I am Rizki Syapytra!!")
+//        }
+        
+            
+        
+//        .padding()
+//    }
 }
 
 #Preview(windowStyle: .automatic) {
